daf721fd646df3a8e8418bd92ad30ddb0483463d
Lars The 18Th <52163725+LarsThe18Th@users.noreply.github.com>
2025-05-18 14:20:19 +0200

    Update Menu 0.1.asm

--- MSX/ASM/DOS/Boot_Menu/Menu 0.1.asm
+++ MSX/ASM/DOS/Boot_Menu/Menu 0.1.asm
@@ -25,62 +25,62 @@ begin:
 
 
 				ld de,menutxt
-				ld c,9								;Print menu TXT
+				ld c,9				;Print menu TXT
 				call 5
 
 
-				ld hl,#fbf0							;Reset Keyboard Buffer
+				ld hl,#fbf0			;Reset Keyboard Buffer
 				ld (#f3fa),hl
 				ld (#f3f8),hl
 
 
 readfkeys:
 				;Read F-Keys loop
-					ld iy,(#fcc0)					;Inter-SlotCall 
-					ld a,6							;Row 6
-					ld ix,#0141						;BIOS CALL GetKey
-					call #1c
+				ld iy,(#fcc0)			;Inter-SlotCall
+				ld a,6				;Row 6
+				ld ix,#0141			;BIOS CALL GetKey
+				call #1c
 
-					cpl
-					or a
-					jr z,readfkeys					;if #ff no key pressed
+				cpl
+				or a
+				jr z,readfkeys			;if #ff no key pressed
 
 				and %11100000
 				rlca
 				rlca
 				rlca
-				jr z,readfkeys						;return if zero
-				
-				
-				cp 1								;Is it F1
-				jr nz,menu2
-					ld bc, prg1
+				jr z,readfkeys			;return if zero
 
-menu2:			cp 2								;Is it F2
+
+				cp 1				;Is it F1
+				jr nz,menu2
+				ld bc, prg1
+
+menu2:				cp 2				;Is it F2
 				jr nz,menu3
-					ld bc, prg2
+				ld bc, prg2
 	
-menu3:			cp 4								;Is it F3
+menu3:				cp 4				;Is it F3
 				jr nz,writekb
-					ld bc, prg3
+				ld bc, prg3
 
 
 writekb:
 				di
-				ld hl,($f3f8)						;Read keyboard buffer write pointer
-				ld ($f3fa),hl						;Clear Keyboard Buffer
+				ld hl,($f3f8)			;Read keyboard buffer write pointer
+				ld ($f3fa),hl			;Clear Keyboard Buffer
 
 loop:
-				ld a,(bc)							;Put text from BC in keyboardbuffer
+				ld a,(bc)			;Put text from BC in keyboardbuffer
 				or a
-				jr z,sendbuff						;Until 0 is detected
+				jr z,sendbuff			;Until 0 is detected
 					ld (hl),a
-					inc hl							;Increase pointer keyboardbuffer
-					inc bc							;Increase pointer BC text string
+					inc hl			;Increase pointer keyboardbuffer
+					inc bc			;Increase pointer BC text string
 				jr loop
 
 sendbuff:
-				ld    ($f3f8),hl					;Write keyboardbuffer
+				ld    ($f3f8),hl		;Write keyboardbuffer
 				ei
 
 ending:
@@ -95,7 +95,7 @@ menutxt:
 				db "[F2] Multi-Mente",10,13
 				db "[F3] Sofarun",10,13,10,13
 				;db 219,219,219,219,219,219,127,
-				db "$" 
+				db "$"
 
 prg1:
 				db "M",13,0
